c15331f3cfa9891abbc3bc9d4156c0b3127b1762
SHMAKET <grachovv12345@gmail.com>
2026-01-05 23:00:47 +0300

    control implemented

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ CRGB leds[NUM_LEDS];
 EffectManager effects(leds);
 GButton touch(BTN_PIN, LOW_PULL, NORM_OPEN);
 
+bool OnFlag = false;
+
 float readBatteryVoltage() {
     uint32_t sum = 0;
     const int samples = 32;
@@ -43,7 +45,7 @@ void setup() {
     Serial.println("=== Registered Effects ===");
     for (uint8_t i = 0; i < effects.getEffectCount(); i++) {
         const EffectDescriptor* desc = effects.getEffectDescriptor(i);
-        Serial.printf("[%d] %s (FPS: %d)\n", i, desc->name, desc->target_fps);
+        Serial.printf("[%d] %s (FPS: %d) ", i, desc->name, desc->target_fps);
         Serial.printf("    Defaults: brightness=%d, speed=%d, k_factor=%d\n",
                     desc->default_params.brightness,
                     desc->default_params.speed,
@@ -57,16 +59,25 @@ void setup() {
 }
 
 void loop() {
+    touch.tick();
+    uint8_t clickCount = touch.hasClicks() ? touch.getClicks() : 0U;
     uint32_t now = millis();
-    
-    if (touch.isClick()) {
-        effects.nextEffect();
-        
-        float vbat = readBatteryVoltage();
-        Serial.printf("Battery voltage: %.3f V\n", vbat);
+
+    if (OnFlag) {
+        if (clickCount == 2U) { effects.prevEffect(); }
+        if (clickCount == 3U) { effects.nextEffect(); }
+        effects.update(now);
+    }
+
+    if (clickCount == 1U) {
+        OnFlag = !OnFlag;
+        if (OnFlag) {
+            float vbat = readBatteryVoltage();
+            Serial.printf("Battery voltage: %.3f V\n", vbat);
+        } else {
+            FastLED.clear();
+        }
     }
     
-    effects.update(now);
     FastLED.show();
-    touch.tick();
 }
